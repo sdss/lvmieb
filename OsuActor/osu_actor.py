facea9e74d57fe260a9aee796930a6256c0020f4
taeeun <taeeun13@khu.ac.kr>
2021-03-20 07:26:55 -0700

    add telemetry command

--- OsuActor/osu_actor.py
+++ OsuActor/osu_actor.py
@@ -1,6 +1,7 @@
 import asyncio
 import OSU_control as osu
 
+from pymodbus3.client.sync import ModbusTcpClient as mbc
 from clu import AMQPActor, command_parser
 
 desi=osu.Controller()
@@ -44,6 +45,29 @@ async def close(command):
     command.finish(shutter="closed")
 
     return
+
+@command_parser.command()
+async def telemetry(command):
+
+    wago_status2, reply = desi.getWAGOPower()
+    wago_status1, reply = desi.getWAGOEnv()
+    if wago_status1 and wago_status2:
+        command.info(text="Temperature & Humidity is:",status={
+                "rhtT1(40001)":desi.sensors['40001'],
+                "rhtRH1(40002)":desi.sensors['40002'],
+                "rhtT2(40003)":desi.sensors['40003'],
+                "rhtRH2(40004)":desi.sensors['40004'],
+                "rtd1(40005)":desi.sensors['40005'],
+                "rtd2(40006)":desi.sensors['40006']
+                })
+        wagoClient = mbc(desi.wagoHost)
+        rd = wagoClient.read_holding_registers(0,12)
+        print(rd.registers)
+
+    else:
+        return command.fail(text=f"ERROR: Did not read sensors/powers")
+
+    return command.finish()
 
 
 class OsuActor(AMQPActor):
