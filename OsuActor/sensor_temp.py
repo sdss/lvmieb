b07b7d8ff7700691268ff37881ce720e5b63a407
taeeun <taeeun13@khu.ac.kr>
2021-03-21 06:46:15 -0700

    delete unnecessary lines

--- OsuActor/sensor_temp.py
+++ OsuActor/sensor_temp.py
@@ -1,35 +1,28 @@
 import OSU_control as osu
 from pymodbus3.client.sync import ModbusTcpClient as mbc
 desi=osu.Controller()
-print(desi.controller_status['wago'])
+
 print(desi.sensors)
-
-#desi.controller_status['wago'] = 'READY'
-#print(desi.controller_status['wago'])
-
-
 
 ######initialization process########
 
 # Populate the self.sensors data structure 
 wago_status1, reply = desi.getWAGOEnv()
 if wago_status1:
-            print("WAGO sensor values read")
+    print("WAGO sensor values read")
 
-        # Populate self.controller_status data structure power values
+# Populate self.controller_status data structure power values
 wago_status2, reply = desi.getWAGOPower()
 if wago_status2:
-            print("WAGO power values read")
+    print("WAGO power values read")
 
-        # If both previous steps returned True, set WAGO status to ready 
+# If both previous steps returned True, set WAGO status to ready 
 if wago_status1 and wago_status2:
-            desi.controller_status['wago'] = 'READY'
+    desi.controller_status['wago'] = 'READY'
 else:
-            print("ERROR: Did not read WAGO sensors/powers")
+    print("ERROR: Did not read WAGO sensors/powers")
 
-print(desi.controller_status['wago'])
-#############################################################
+############################################################
 
-#desi.getWAGOEnv()
 print(desi.sensors)
 
