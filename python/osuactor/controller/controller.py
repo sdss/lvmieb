c28d91ac1cdc39dbf25e0119e6ae4164c689fc63
changgonKim <changgonkim@khu.ac.kr>
2021-04-04 21:11:49 -0700

    exposure shutter control is working!

--- python/osuactor/controller/controller.py
+++ python/osuactor/controller/controller.py
@@ -50,14 +50,14 @@ class OsuController(Device):
 
         sclStr = sclHead + c_message.upper() + sclTail
 
-        print(f'Send: {message!r}')
+#        print(f'Send: {message!r}')
         self.writer.write(sclStr.encode())
         await self.writer.drain()
 
         status, Reply = await self.receive_status(self.reader)
-        print('status : ', status)
-        print('reply : ', Reply)
-        print('Close the connection')
+#        print('status : ', status)
+#        print('reply : ', Reply)
+#        print('Close the connection')
         self.writer.close()
         await self.writer.wait_closed()
 
@@ -72,116 +72,13 @@ class OsuController(Device):
             sclReply = recStr[2:-1]
 
             if sclReply[:4] == 'DONE':
-                print(sclReply[:4])
+#                print(sclReply[:4])
                 return True, sclReply
             if sclReply[:3] == 'ERR':
-                print(sclReply[:3])
+#                print(sclReply[:3])
                 return True, sclReply
             if sclReply[:2] == 'IS':
-                print(sclReply[:2])
+#                print(sclReply[:2])
                 return True, sclReply
-
-
-
-
-
-"""
-    async def send_command(self, message: str):
- #       c_message = expCmds[message]
-        sclHead = chr(0)+chr(7)
-        sclTail = chr(13)
-        sclStr = sclHead + message.upper() + sclTail
-        self.write(sclStr)
-        #await self.drain()
-
-#        status, Reply = await self._listen(self)
-#        print('status : ', status)
-#        print('reply : ', Reply)
-#        return status
-"""
-"""
-    async def receive_status(areader):
-        KeepGoing = True
-        while KeepGoing:
-            sclReply = ""
-            data = await areader.read(4096)
-            recStr = data.decode()
-            sclReply = recStr[2:-1]
-
-            if sclReply[:4] == 'DONE':
-                print(sclReply[:4])
-                return True, sclReply
-            if sclReply[:3] == 'ERR':
-                print(sclReply[:3])
-                return True, sclReply
-            if sclReply[:2] == 'IS':
-                print(sclReply[:2])
-                return True, sclReply
-"""
-
-#async def test():
-#    my_device = OsuController()
-    #await my_device.start()
-    #if my_device.is_connected():
-    #    print("connection is open")
-    #else:
-    #    print("connection is close")
-
-#    result = False
-#    await my_device.send_message('open')
-
- #   if result:
-  #      print("shutter open")
-   # else:
-    #    print("open fail")
-
-    #await my_device.stop()
-    #if my_device.is_connected() != True:
-    #    print("connection closed")
-
-#asyncio.run(test())
-
-
-
-"""
-async def send_message(message):
-    reader, writer = await asyncio.open_connection(
-        '10.7.45.27', 7776)
-
-    sclHead = chr(0)+chr(7)
-    sclTail = chr(13)
-
-    sclStr = sclHead + message.upper() + sclTail
-
-    print(f'Send: {message!r}')
-    writer.write(sclStr.encode())
-    await writer.drain()
-
-    status, Reply = await receive_status(reader)
-    print('status : ', status)
-    print('reply : ', Reply)
-    print('Close the connection')
-    writer.close()
-    await writer.wait_closed()
-
-async def receive_status(areader):
-    KeepGoing = True
-    while KeepGoing:
-            sclReply = ""
-            data = await areader.read(4096)
-            recStr = data.decode()
-            sclReply = recStr[2:-1]
-
-            if sclReply[:4] == 'DONE':
-                print(sclReply[:4])
-                return True, sclReply
-            if sclReply[:3] == 'ERR':
-                print(sclReply[:3])
-                return True, sclReply
-            if sclReply[:2] == 'IS':
-                return True, sclReply
-"""
-#asyncio.run(send_message('QX4'))
-
 
 
