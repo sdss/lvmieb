db2919676a8a8d74304fb982ba4ed97a524cc240
José Sánchez-Gallego <gallegoj@uw.edu>
2023-03-04 10:25:19 -0300

    Fix cli_coro in CLI for now

--- python/lvmieb/__main__.py
+++ python/lvmieb/__main__.py
@@ -7,15 +7,29 @@
 
 from __future__ import annotations
 
+import asyncio
+import functools
 import os
 
 import click
 from click_default_group import DefaultGroup
 
-from clu.tools import cli_coro as cli_coro_lvm
 from sdsstools.daemonizer import DaemonGroup
 
 from lvmieb.actor.actor import IEBActor
+
+
+def cli_coro(f):
+    """Decorator function that allows defining coroutines with click."""
+
+    if hasattr(asyncio, "coroutine"):
+        f = getattr(asyncio, "coroutine")(f)
+
+    def wrapper(*args, **kwargs):
+        loop = asyncio.get_event_loop()
+        return loop.run_until_complete(f(*args, **kwargs))
+
+    return functools.update_wrapper(wrapper, f)
 
 
 @click.group(cls=DefaultGroup, default="actor", default_if_no_args=True)
@@ -27,40 +41,33 @@ from lvmieb.actor.actor import IEBActor
     help="Path to the user configuration file.",
 )
 @click.option(
-    "-r",
-    "--rmq_url",
-    "rmq_url",
-    default=None,
-    type=str,
-    help="rabbitmq url, eg: amqp://guest:guest@localhost:5672/",
-)
-@click.option(
     "-v",
     "--verbose",
     count=True,
     help="Debug mode. Use additional v for more details.",
 )
 @click.pass_context
-def lvmieb(ctx, config_file, rmq_url, verbose):
+def lvmieb(ctx, config_file, verbose):
     """LVM Electronics Box Controller."""
 
-    ctx.obj = {"verbose": verbose, "config_file": config_file, "rmq_url": rmq_url}
+    ctx.obj = {"verbose": verbose, "config_file": config_file}
 
 
 @lvmieb.group(cls=DaemonGroup, prog="lvmieb_actor", workdir=os.getcwd())
 @click.pass_context
-@cli_coro_lvm
+@cli_coro
 async def actor(ctx):
     """Runs the actor."""
 
     default_config_file = os.path.join(os.path.dirname(__file__), "etc/lvmieb.yml")
     config_file = ctx.obj["config_file"] or default_config_file
 
-    lvmieb_obj = IEBActor.from_config(config_file, url=ctx.obj["rmq_url"])
+    lvmieb_obj = IEBActor.from_config(config_file)
 
     if ctx.obj["verbose"]:
-        lvmieb_obj.log.fh.setLevel(0)
         lvmieb_obj.log.sh.setLevel(0)
+        if lvmieb_obj.log.fh:
+            lvmieb_obj.log.fh.setLevel(0)
 
     await lvmieb_obj.start()
     await lvmieb_obj.run_forever()
