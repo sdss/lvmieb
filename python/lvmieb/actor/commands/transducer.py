150ceea49637f4700d564083accdd66ea4b18353
José Sánchez-Gallego <gallegoj@uw.edu>
2023-09-13 22:51:02 +0000

    Ugly code to try in transducer command

--- python/lvmieb/actor/commands/transducer.py
+++ python/lvmieb/actor/commands/transducer.py
@@ -26,25 +26,28 @@ async def status(
 ):
     """Returns the status of transducer."""
 
-    tasks = []
-
     pres_result = {}
+
     camera_order = []
-
-    for controller_name in controllers:
-        if spectro is not None and controller_name != spectro:
-            continue
-        controller = controllers[controller_name]
-        for camera, pressure_transducer in controller.pressure.items():
-            tasks.append(pressure_transducer.read_pressure())
-            tasks.append(pressure_transducer.read_temperature())
-            camera_order.append(camera)
-
     pres_list = []
+    error = False
 
     NRETRIES = 3
     for iretry in range(NRETRIES):
+        camera_order = []
+        tasks = []
         pres_list = []
+        error = False
+
+        for controller_name in controllers:
+            if spectro is not None and controller_name != spectro:
+                continue
+            controller = controllers[controller_name]
+            for camera, pressure_transducer in controller.pressure.items():
+                tasks.append(pressure_transducer.read_pressure())
+                tasks.append(pressure_transducer.read_temperature())
+                camera_order.append(camera)
+
         for task in tasks:
             try:
                 # We avoid doing this with a gather to prevent multiple connections to
@@ -52,18 +55,21 @@ async def status(
                 result = await task
                 pres_list.append(result)
             except Exception as err:
-                if iretry < NRETRIES - 1:
-                    command.warning(f"Failed getting pressure status: {err}. Retrying.")
-                    continue
-        break
+                error = err
+
+        if error is not False and iretry < NRETRIES - 1:
+            command.warning(f"Failed getting pressure status: {error}. Retrying.")
+            continue
+        else:
+            command.error(f"Failed getting pressure status: {error}")
+
+    if len(pres_list) == 0 or error:
+        return command.fail(error="Unable to retrieve any pressure data.")
 
     for ii, camera in enumerate(camera_order):
         camera_results = pres_list[2 * ii : 2 * ii + 2]
         for jj, measurement in enumerate(["pressure", "temperature"]):
             camera_result = camera_results[jj]
-            if camera_result is False or isinstance(camera_result, Exception):
-                command.warning(f"Failed getting {camera} {measurement}.")
-                camera_result = -999.0
             pres_result[f"{camera}_{measurement}"] = camera_result
 
     command.finish(transducer=pres_result)
